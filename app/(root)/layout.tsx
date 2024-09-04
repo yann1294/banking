dc176b53d1934fa88316234632ce0cf05cbd3bbf
Mimi_Son <mimison@192.168.0.108>
2024-09-05 01:29:24 +0530

    implement the Sidebar to the root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import SideBar from "@/components/SideBar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Abraham Yann", lastName: "Aguidissou" };
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <SideBar user={loggedIn} />
       {children}
     </main>
   );
