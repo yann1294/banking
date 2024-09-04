b94e4936f656c78cfe0378f36365eb3ad1f17237
Mimi_Son <mimison@192.168.0.108>
2024-09-04 17:27:25 +0530

    implement home page initial components

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import CountUp from "react-countup";
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <div className="w-full">
+      <CountUp end={amount} decimal="," prefix="$" decimals={2} />
+    </div>
+  );
+};
+
+export default AnimatedCounter;
