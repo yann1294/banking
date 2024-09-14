f4eda3c04219e8eeb93bcf2048afe7cb9f3b3fde
Mimi_Son <mimison@192.168.0.105>
2024-09-14 10:03:22 +0530

    begining plaid services integration

---
+++ components/PlaidLink.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+  return (
+    <>
+      {variant === "primary" ? (
+        <Button className="plaidlink-primary" onClick={() => {}}>
+          {" "}
+          Connect bank{" "}
+        </Button>
+      ) : variant === "ghost" ? (
+        <Button>Connect bank</Button>
+      ) : (
+        <Button>Connect bank</Button>
+      )}
+    </>
+  );
+};
+
+export default PlaidLink;
