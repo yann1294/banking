f68b24bbf4264b26953ca0090458b4241249adc2
Mimi_Son <mimison@192.168.0.104>
2024-09-15 20:42:39 +0530

    Implement plaid link and Dwolla

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,11 +1,63 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Button } from "./ui/button";
+import {
+  PlaidLinkOnSuccess,
+  PlaidLinkOptions,
+  usePlaidLink,
+} from "react-plaid-link";
+import { useRouter } from "next/navigation";
+import {
+  createLinkToken,
+  exchangePublicToken,
+} from "@/lib/actions/user.actions";
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+  const router = useRouter();
+
+  const [token, setToken] = useState<string>("");
+
+  useEffect(() => {
+    const getLinkToken = async () => {
+      try {
+        const data = await createLinkToken(user);
+        if (data && data.linkToken) {
+          setToken(data.linkToken);
+        } else {
+          console.error("Link token not found in the response");
+        }
+      } catch (error) {
+        console.error("Error fetching link token:", error);
+      }
+    };
+    getLinkToken();
+  }, [user]);
+
+  const onSuccess = useCallback<PlaidLinkOnSuccess>(
+    async (public_token: string) => {
+      await exchangePublicToken({
+        publicToken: public_token,
+        user,
+      });
+      router.push("/");
+    },
+    [user],
+  );
+
+  const config: PlaidLinkOptions = {
+    token,
+    onSuccess,
+  };
+
+  const { open, ready } = usePlaidLink(config);
+
   return (
     <>
       {variant === "primary" ? (
-        <Button className="plaidlink-primary" onClick={() => {}}>
+        <Button
+          className="plaidlink-primary"
+          onClick={() => open()}
+          disabled={!ready}
+        >
           {" "}
           Connect bank{" "}
         </Button>
